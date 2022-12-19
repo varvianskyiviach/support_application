203866c96a7ae2458da7fb1c2056d1c4d7b19534
varvianskyiviach <vyacheslav.idz@gmail.com>
2022-12-19 14:29:05 +0200

    urls commit

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,9 +1,25 @@
+import json
+from dataclasses import asdict, dataclass
+
 from django.http import HttpResponse
 from django.urls import path
 
 
+@dataclass
+class Sport:
+    kind: str
+    qulity_players: int
+    famous_team: list
+
+
 def get_result(variebles):
-    return HttpResponse("Hello")
+    football = Sport(
+        kind="Football",
+        qulity_players=11,
+        famous_team=["Barselona", "Milan", "Manchester United"],
+    )
+    content = json.dumps(asdict(football))
+    return HttpResponse(content)
 
 
 urlpatterns = [
