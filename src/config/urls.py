7e0ea4af29705dca3da2c3b70f5ce049dff8780c
varvianskyiviach <vyacheslav.idz@gmail.com>
2022-12-20 01:06:30 +0200

    alpha vantage api key

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,27 +1,5 @@
-import json
-from dataclasses import asdict, dataclass
-
-from django.http import HttpResponse
-from django.urls import path
-
-
-@dataclass
-class Sport:
-    kind: str
-    qulity_players: int
-    famous_team: list
-
-
-def get_result(variebles):
-    football = Sport(
-        kind="Football",
-        qulity_players=11,
-        famous_team=["Barselona", "Milan", "Manchester United"],
-    )
-    content = json.dumps(asdict(football))
-    return HttpResponse(content)
-
+from django.urls import include, path
 
 urlpatterns = [
-    path("result/", get_result),
+    path("exchange-rates/", include("exchange_rates.urls")),
 ]
---
+++ src/exchange_rates/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from exchange_rates.api import convert
+
+urlpatterns = [
+    path("convert/", convert),
+]
