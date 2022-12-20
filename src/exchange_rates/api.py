0772288c87897b57e74cf365e6c8a713cd3c9d43
varvianskyiviach <vyacheslav.idz@gmail.com>
2022-12-20 16:24:21 +0200

    alphavantage update

--- src/exchange_rates/api.py
+++ src/exchange_rates/api.py
@@ -1,28 +1,20 @@
+import json
+
 import requests
 from django.conf import settings
 from django.http import JsonResponse
-from pydantic import BaseModel, Field
 
-# https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=demo
-
-
-class ExchangeRatesResults(BaseModel):
-    from_currency: str = Field(alias="1. From_Currency Code")
-    to_currency: str = Field(alias="3. To_Currency Code")
-    rate: str = Field(alias="5. Exchange Rate")
-
-
-class AlphavantageResponse(BaseModel):
-    results: ExchangeRatesResults = Field(alias="Realtime Currency Exchange Rate")
+from exchange_rates.service import AlphavantageResponse, PostRequest
 
 
 def convert(request):
-    from_currency = "USD"
-    to_currency = "UAH"
+    request = PostRequest(**json.loads(request.body))
+    from_currency = request.from_
+    to_currency = request.to_
     url = (
         f"{settings.ALPHA_VANTAGE_BASE_URL}/query?function=CURRENCY_EXCHANGE_RATE&"
         f"from_currency={from_currency}&to_currency={to_currency}&apikey={settings.ALPHA_VANTAGE_API_KEY}"
     )
     response = requests.get(url)
     alphavantage_response = AlphavantageResponse(**response.json())
-    return JsonResponse(alphavantage_response.dict())
+    return JsonResponse(alphavantage_response.results.dict())
