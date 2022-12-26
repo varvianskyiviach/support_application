15e4c39d5e6a78b615fab421190e1dd1838f6ae8
Varvianskyi Viacheslav <vyacheslav.idz@gmail.com>
2022-12-26 14:09:01 +0200

    add custom user model

--- src/config/settings.py
+++ src/config/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "exchange_rates",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -104,6 +105,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Set our user model
+
+AUTH_USER_MODEL = "users.User"
 
 # Exchange rates service settings (Alpha Vantage)
 ALPHA_VANTAGE_BASE_URL = getenv(
