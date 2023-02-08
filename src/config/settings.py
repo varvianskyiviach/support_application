63fd65c62ad9822ab07d5c183af1121c6c7ee506
varvianskyiviach <vyacheslav.idz@gmail.com>
2023-02-08 11:12:19 +0200

    tickets permissions

--- src/config/settings.py
+++ src/config/settings.py
@@ -129,9 +129,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
 }
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
