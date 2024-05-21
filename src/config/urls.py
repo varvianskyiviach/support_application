fff3e07c5173bd9ca467af0ca252c7cc58af2a59
varvianskyiviach <vyacheslav.idz@gmail.com>
2024-05-21 23:49:21 +0300

    some commit

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,13 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-<<<<<<< HEAD
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-=======
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
->>>>>>> 9384e17 (diagram & openapi is instaled)
 from rest_framework import permissions
 
 schema_view = get_schema_view(
@@ -29,11 +24,8 @@ urlpatterns: list = [
     path("users/", include("users.urls")),
     path("", include("core.urls")),
     path("auth/", include("authentication.urls")),
-<<<<<<< HEAD
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-=======
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name='schema-swagger-ui')
->>>>>>> 9384e17 (diagram & openapi is instaled)
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
 
 if settings.DEBUG:
